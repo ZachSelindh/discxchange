8b6c806a0250ac007abed49343bc1812734325ac
Zach Selindh <zachselindh@gmail.com>
2020-02-16 10:14:15 -0600

    Adding models and schemas.

---
+++ models/disc/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const discSchema = new Schema({
+  title: { type: String, required: true },
+  year: { type: Number, require: true },
+  season: { type: String, required: false },
+  description: { type: String, required: true },
+  price: { type: Number, required: true }
+});
+
+const Disc = mongoose.model("Disc", discSchema);
+
+module.exports = Disc;
